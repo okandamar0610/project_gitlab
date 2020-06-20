710752d618486417fdb93f21c25dce81bce3240a
okandamar0610 <okandamar@yahoo.com>
2020-06-20 19:14:28 +0000

    added

--- Instance.tf
+++ Instance.tf
@@ -9,8 +9,8 @@ resource "aws_security_group" "allow_tls" {
 
   ingress {
     description = "TLS from VPC"
-    from_port   = 443
-    to_port     = 443
+    from_port   = 80
+    to_port     = 80
 
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
